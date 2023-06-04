f6b2e6e1693b0eeb18ae108d0a871be24b904de7
gourami32 <127053477+gourami32@users.noreply.github.com>
2023-06-04 21:19:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,18 @@
-// Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
 #include  "bst.h"
-
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> BSTtree;
+  std::ifstream file(filename);
+  std::string result;
+  while (!file.eof()) {
+    char s = file.get();
+    if (s >= 'A' && s <= 'Z')
+        s = s + ('a' - 'A');
+    if (s >= 'a' && s <= 'z') {
+      result = result + s;
+    } else {
+      BSTtree.add(result);
+      result.clear();
+    }
+  }
+  return BSTtree;
 }
